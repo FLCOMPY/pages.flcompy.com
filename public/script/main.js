0435d5e65756970ec16a53d822e259dc7f5361a0
Henry Rodrigues <henry.rodrigues@flcompy.com>
2022-02-23 11:09:30 -0300

    Add files via upload

--- public/script/main.js
+++ public/script/main.js
@@ -1,28 +1,45 @@
 menuService();
 yearService();
+scrollinterService();
+
 
 function menuService() {
-    const nav = document.querySelector("#header ._fl-menu");
-    const toggle = document.querySelectorAll("._fl-menu .toggle");
+  const nav = document.querySelector("#header ._fl-menu");
+  const toggle = document.querySelectorAll("._fl-menu .toggle");
 
-    for (const element of toggle) {
-        element.addEventListener("click", function () {
-            nav.classList.toggle("show");
-        });
-    }
+  for (const element of toggle) {
+    element.addEventListener("click", function () {
+      nav.classList.toggle("show");
+    });
+  }
 
-    const links = document.querySelectorAll("nav ul li a");
+  const links = document.querySelectorAll("nav ul li a");
 
-    for (const link of links) {
-        link.addEventListener("click", function () {
-            nav.classList.remove("show");
-        });
-    }
+  for (const link of links) {
+    link.addEventListener("click", function () {
+      nav.classList.remove("show");
+    });
+  }
 }
 
 function yearService() {
-    const year = document.getElementById("copyright");
-    const yearCurrent = new Date();
+  const year = document.getElementById("copyright");
+  const yearCurrent = new Date();
 
-    year.innerHTML = yearCurrent.getFullYear();
+  year.innerHTML = yearCurrent.getFullYear();
 }
+
+function scrollinterService() {
+    $('.nav_internal a[href^="#"]').on("click", function (e) {
+        e.preventDefault();
+        var id = $(this).attr("href"), targetOffset = $(id).offset().top;
+
+        $("html, body").animate(
+            {
+                scrollTop: targetOffset - 100,
+            },
+            500
+        );
+    });
+}
+
